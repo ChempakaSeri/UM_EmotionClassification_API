b1d95d8f27abc370fbdd8c7b0332ab290cbdf9b3
ameerzulkarnain <ameerzulkarnain@gmail.com>
2022-01-22 00:06:34 +0800

    change init_v2

--- init_v2.py
+++ init_v2.py
@@ -43,6 +43,19 @@ db_name = 'tweep'
 retName = ['Predicted_emotion','Predicted_emotion_value', 'Probability_afraid','Probability_anger','Probability_bored','Probability_excited','Probability_happy', 'Probability_relax', 'Probability_sad']
 
 feature = []
+    
+pred_models={'word2seq_cnn' : load_model('./Models/word2seq_cnn.hdf5'),
+                'word2vec_cnn' : load_model('./Models/word2vec_cnn.hdf5'),
+                'word2seq_cnn_birnn_bilstm' : load_model('./Models/word2seq_cnn_birnn_bilstm.hdf5'),
+                'word2vec_cnn_birnn_bilstm' : load_model('./Models/word2vec_cnn_birnn_bilstm.hdf5')}
+
+## Make prediction function
+for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
+    pred_models[model]._make_predict_function()
+
+## Loading the Keras Tokenizer sequence file
+with open('./pickle/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 
 @app.route('/', methods=['GET'])
@@ -153,27 +166,7 @@ def predict(text):
     
     return(return_dict)
 
-def main():
-    ## Load the Keras-Tensorflow models into a dictionary
-    global pred_models 
-    
-    pred_models={'word2seq_cnn' : load_model('./Models/word2seq_cnn.hdf5'),
-                 'word2vec_cnn' : load_model('./Models/word2vec_cnn.hdf5'),
-                 'word2seq_cnn_birnn_bilstm' : load_model('./Models/word2seq_cnn_birnn_bilstm.hdf5'),
-                 'word2vec_cnn_birnn_bilstm' : load_model('./Models/word2vec_cnn_birnn_bilstm.hdf5')}
-    
-    ## Make prediction function
-    for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
-        pred_models[model]._make_predict_function()
-    
-    ## Loading the Keras Tokenizer sequence file
-    global tokenizer
-    with open('./pickle/tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-        
-    app.run(host='localhost', port=5000)
-
 if __name__ == '__main__':
-    main()
+    app.run(host='localhost', port=5000)
 
 
