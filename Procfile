aad0b723e790336844be4d809162c596663ef1f2
ameerzulkarnain <ameerzulkarnain@gmail.com>
2022-01-21 20:44:10 +0800

    add DigitalOcean setup

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn init_v2:app
--- init_v2.py
+++ init_v2.py
@@ -44,9 +44,6 @@ retName = ['Predicted_emotion','Predicted_emotion_value', 'Probability_afraid','
 
 feature = []
 
-## Load the Keras-Tensorflow models into a dictionary
-
-
 
 @app.route('/', methods=['GET'])
 def index():
@@ -73,12 +70,6 @@ def predict(text):
 
     return_dict={}
     return_list={}
-
-    ## Loading the Keras Tokenizer sequence file
-    global tokenizer
-    with open('./pickle/tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-
     
     ## Tokkenizing test data and create matrix
     list_tokenized_test = tokenizer.texts_to_sequences([text])
@@ -97,14 +88,6 @@ def predict(text):
          'Ari_tweet':str(ari),
          'Char_tweet':str(char)}
     )
-
-    pred_models={'word2seq_cnn' : load_model('./Models/word2seq_cnn.hdf5'),
-                'word2vec_cnn' : load_model('./Models/word2vec_cnn.hdf5'),
-                'word2seq_cnn_birnn_bilstm' : load_model('./Models/word2seq_cnn_birnn_bilstm.hdf5'),
-                'word2vec_cnn_birnn_bilstm' : load_model('./Models/word2vec_cnn_birnn_bilstm.hdf5')}
-
-    for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
-        pred_models[model]._make_predict_function()
     
     for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
         x_test = keras_seq.pad_sequences(list_tokenized_test, 
@@ -170,27 +153,27 @@ def predict(text):
     
     return(return_dict)
 
-# def main():
-#     ## Load the Keras-Tensorflow models into a dictionary
-#     global pred_models 
+def main():
+    ## Load the Keras-Tensorflow models into a dictionary
+    global pred_models 
     
-#     pred_models={'word2seq_cnn' : load_model('./Models/word2seq_cnn.hdf5'),
-#                  'word2vec_cnn' : load_model('./Models/word2vec_cnn.hdf5'),
-#                  'word2seq_cnn_birnn_bilstm' : load_model('./Models/word2seq_cnn_birnn_bilstm.hdf5'),
-#                  'word2vec_cnn_birnn_bilstm' : load_model('./Models/word2vec_cnn_birnn_bilstm.hdf5')}
+    pred_models={'word2seq_cnn' : load_model('./Models/word2seq_cnn.hdf5'),
+                 'word2vec_cnn' : load_model('./Models/word2vec_cnn.hdf5'),
+                 'word2seq_cnn_birnn_bilstm' : load_model('./Models/word2seq_cnn_birnn_bilstm.hdf5'),
+                 'word2vec_cnn_birnn_bilstm' : load_model('./Models/word2vec_cnn_birnn_bilstm.hdf5')}
     
-#     ## Make prediction function
-#     for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
-#         pred_models[model]._make_predict_function()
+    ## Make prediction function
+    for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
+        pred_models[model]._make_predict_function()
     
-#     ## Loading the Keras Tokenizer sequence file
-#     global tokenizer
-#     with open('./pickle/tokenizer.pickle', 'rb') as handle:
-#         tokenizer = pickle.load(handle)
+    ## Loading the Keras Tokenizer sequence file
+    global tokenizer
+    with open('./pickle/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
         
-#     app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000)
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
 
