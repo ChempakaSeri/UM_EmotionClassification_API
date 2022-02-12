af9f597566b4a81f8fd8c98ad624705e04e34315
ameerzulkarnain <ameerzulkarnain@gmail.com>
2022-02-12 10:05:25 +0800

    test new model

--- init_v2.py
+++ init_v2.py
@@ -8,6 +8,11 @@ from tensorflow.python.keras.models import load_model
 from flask import request, jsonify
 import warnings
 
+#   UM_EmotionIdentification_API
+import xgboost as xgb
+import pandas as pd
+import json
+
 #   Import ARI
 import textstat
 
@@ -43,46 +48,56 @@ db_name = 'tweep'
 retName = ['Predicted_emotion','Predicted_emotion_value', 'Probability_afraid','Probability_anger','Probability_bored','Probability_excited','Probability_happy', 'Probability_relax', 'Probability_sad']
 
 feature = []
+
+header = ['total_tweet','afraid_percent','anger_percent','bored_percent','excited_percent','happy_percent','relax_percent','sad_percent','avg_length','avg_ari','avg_char','std_dev','afraid_prob','anger_prob','bored_prob','excited_prob','happy_prob','relax_prob','sad_prob']
+
+retName_v2 = ['Probability_afraid','Probability_anger','Probability_bored','Probability_excited','Probability_happy', 'Probability_relax', 'Probability_sad']
     
-pred_models={'word2seq_cnn' : load_model('./Models/word2seq_cnn.hdf5'),
-                'word2vec_cnn' : load_model('./Models/word2vec_cnn.hdf5'),
-                'word2seq_cnn_birnn_bilstm' : load_model('./Models/word2seq_cnn_birnn_bilstm.hdf5'),
-                'word2vec_cnn_birnn_bilstm' : load_model('./Models/word2vec_cnn_birnn_bilstm.hdf5')}
+pred_models={'word2seq_cnn' : load_model('./Models//word2seq_cnn.hdf5'),
+                'word2vec_cnn' : load_model('./Models//word2vec_cnn.hdf5'),
+                'word2seq_cnn_birnn_bilstm' : load_model('./Models//word2seq_cnn_birnn_bilstm.hdf5'),
+                'word2vec_cnn_birnn_bilstm' : load_model('./Models//word2vec_cnn_birnn_bilstm.hdf5')}
 
 ## Make prediction function
-for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
+for model in [model[:-5]for model in os.listdir('./Models')]:
     pred_models[model]._make_predict_function()
 
 ## Loading the Keras Tokenizer sequence file
 with open('./pickle/tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-
 @app.route('/', methods=['GET'])
 def index():
     return "<h1>Hello</h1>"
 
 ## Main API get hook function
-@app.route('/api/v1/emotion', methods=['GET'])
+@app.route('/api/v1/emotion', methods=['POST'])
 def api_sentiment():
     global error
     error = False
     
-    if 'text' in request.args:
-        text = str(request.args['text'])
+    if 'text' in request.json:
+        text = request.json['text']
+        print (text,flush=True)
         if text == '':
             return "Error: No text provideed. Please specify a text."
         result = predict(text)
         return(jsonify(result))
+    
+    elif 'data' in request.json:
+        data = request.json['data']
+        if data == '':
+            return "Error: No data provided. Please specify your data."
+        analyse_result = analyse(data)
+        return(jsonify(analyse_result))
+    
     else:
         error = True
         return "Error: No text field provided. Please specify a text."
 
 def predict(text):
     global pred_models
-
     return_dict={}
-    return_list={}
     
     ## Tokkenizing test data and create matrix
     list_tokenized_test = tokenizer.texts_to_sequences([text])
@@ -100,9 +115,9 @@ def predict(text):
         {'Length_tweet':str(length),
          'Ari_tweet':str(ari),
          'Char_tweet':str(char)}
-    )
+    ) 
     
-    for model in [model[:-5]for model in os.listdir('./Models')[1:]]:
+    for model in [model[:-5]for model in os.listdir('./Models')]:
         x_test = keras_seq.pad_sequences(list_tokenized_test, 
                                          maxlen=INPUT_SIZE[model],
                                          padding='post')
@@ -165,5 +180,72 @@ def predict(text):
              }})
     
     return(return_dict)
+
+def analyse(data):
+    joblib_model = xgb.Booster({'nthread':4})
+    
+    joblib_model.load_model('./EIM_Model/xgboost_19.pkl')
+    
+    return_dict = {}
+    
+    print (request.json, flush=True)
+    print(data,flush=True)
+    feature = [float(i) for i in data.split(',')]
+
+    return_dict = {}
+
+    values = pd.DataFrame( [feature],
+                            columns=header,
+                            dtype=float,
+                            index=['input']
+                            )
+
+    input_variable = xgb.DMatrix(values)
+
+    afraid_probability = joblib_model.predict(input_variable)[0][0]
+    anger_probability = joblib_model.predict(input_variable)[0][1]
+    bored_probability = joblib_model.predict(input_variable)[0][2]
+    excited_probability = joblib_model.predict(input_variable)[0][3]
+    happy_probability = joblib_model.predict(input_variable)[0][4]
+    relax_probability = joblib_model.predict(input_variable)[0][5]
+    sad_probability = joblib_model.predict(input_variable)[0][6]
+
+
+    return_dict.update({ #word2seq_cnn, word2vec_cnn, ...
+        retName_v2[0]:str(afraid_probability), 
+        retName_v2[1]:str(anger_probability),
+        retName_v2[2]:str(bored_probability),
+        retName_v2[3]:str(excited_probability),
+        retName_v2[4]:str(happy_probability),
+        retName_v2[5]:str(relax_probability),
+        retName_v2[6]:str(sad_probability)
+    })
+
+    print (return_dict,flush=True)
+    return (return_dict)
+
+
+# def main():
+#     ## Load the Keras-Tensorflow models into a dictionary
+#     global pred_models 
+    
+#     pred_models={'word2seq_cnn' : load_model('C:/Users/Ameer/Documents/UM_EmotionClassification_API/Models/word2seq_cnn.hdf5'),
+#                  'word2vec_cnn' : load_model('C:/Users/Ameer/Documents/UM_EmotionClassification_API/Models/word2vec_cnn.hdf5'),
+#                  'word2seq_cnn_birnn_bilstm' : load_model('C:/Users/Ameer/Documents/UM_EmotionClassification_API/Models/word2seq_cnn_birnn_bilstm.hdf5'),
+#                  'word2vec_cnn_birnn_bilstm' : load_model('C:/Users/Ameer/Documents/UM_EmotionClassification_API/Models/word2vec_cnn_birnn_bilstm.hdf5')}
+    
+#     ## Make prediction function
+#     for model in [model[:-5]for model in os.listdir('C:/Users/Ameer/Documents/UM_EmotionClassification_API/Models')]:
+#         pred_models[model]._make_predict_function()
+    
+#     ## Loading the Keras Tokenizer sequence file
+#     global tokenizer
+#     with open('C:/Users/Ameer/Documents/UM_EmotionClassification_API/tokenizer.pickle', 'rb') as handle:
+#         tokenizer = pickle.load(handle)
+        
+#     app.run(host='localhost', port=5060)
+
+# if __name__ == '__main__':
+#     app.run(host='localhost', port=5000)
 
 
